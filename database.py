3f950141cc78fe1298c7dce472c35f7284a26a1a
Ben Taylor <taybenlor@gmail.com>
2012-01-17 17:28:20 +1100

    indentation error... lol

--- database.py
+++ database.py
@@ -3,13 +3,13 @@ import urlparse, os
 
 if __name__ == "__main__":
     url = urlparse.urlparse(os.environ['DATABASE_URL'])
-            config = {
-                'NAME':     url.path[1:],
-                'USER':     url.username,
-                'PASSWORD': url.password,
-                'HOST':     url.hostname,
-                'PORT':     url.port
-            }
+    config = {
+        'NAME':     url.path[1:],
+        'USER':     url.username,
+        'PASSWORD': url.password,
+        'HOST':     url.hostname,
+        'PORT':     url.port
+    }
 
     conn = DBAPI.connect(host=config['HOST'], user=config['USER'], password=config['NAME'])
     cur = conn.cursor()
