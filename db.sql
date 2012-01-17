9753a222fb26e554b1925a945fa9697c93e283d7
Ben Taylor <taybenlor@gmail.com>
2012-01-17 17:53:09 +1100

    Convert id integers to id serial

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 drop table if exists users;
 
 create table users(
-    id integer primary key auto_increment,
+    id serial primary key,
     username text not null,
     password text not null,
     email text not null,
@@ -12,29 +12,29 @@ create table users(
 drop table if exists posts;
 
 create table posts(
-    id integer primary key auto_increment,
+    id serial primary key,
     title text not null,
     description text,
     time datetime not null,
     comments integer not null default 0,
-    user_id integer not null,
-    foreign key (user_id) references users (user_id),
+    user_id serial not null,
+    foreign key(user_id) references users(user_id),
     unique (title)
 );
 drop table if exists comments;
 create table comments(
-    id integer primary key auto_increment,
-    post_id integer not null,
-    user_id integer not null,
+    id serial primary key,
+    post_id serial not null,
+    user_id serial not null,
     description text,
     agree boolean not null,
-    foreign key (user_id) references users(id),
-    foreign key (post_id) references posts(id)
+    foreign key(user_id) references users(id),
+    foreign key(post_id) references posts(id)
 );
 drop table if exists followers;
 create table followers(
-	follower_id integer not null,
-	followee_id integer not null,
-	foreign key (follower_id) references user(id),
-	foreign key (followee_id) references user(id)
+	follower_id serial not null,
+	followee_id serial not null,
+	foreign key(follower_id) references user(id),
+	foreign key(followee_id) references user(id)
 );
